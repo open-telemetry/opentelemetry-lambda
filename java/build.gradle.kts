d9dbf2289945a78f58afaf99efde706a86c1c2db
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-03-15 15:40:06 +0900

    Fix java version for awssdk-autoconfigure

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,8 +1,8 @@
 subprojects {
     plugins.withId("java") {
         configure<JavaPluginConvention> {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
 
         dependencies {
