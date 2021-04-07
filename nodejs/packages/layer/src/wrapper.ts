508977e6ef6d8400fe1eda1d474295ee94c1a06b
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-07 17:14:09 +0900

    feat: add nodejs layer and sample app

---
+++ nodejs/packages/layer/src/wrapper.ts
@@ -0,0 +1,22 @@
+import { NodeTracerProvider } from '@opentelemetry/node';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-awslambda';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
+
+console.log('Registering OpenTelemetry');
+
+const provider = new NodeTracerProvider();
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+registerInstrumentations({
+  instrumentations: [
+    new AwsInstrumentation({
+      suppressInternalInstrumentation: true,
+    }),
+    new AwsLambdaInstrumentation(),
+  ],
+  tracerProvider: provider,
+});
+
+provider.register();
