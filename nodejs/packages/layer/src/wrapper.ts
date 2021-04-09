a745cb17f79be9f3110bf4a1d58c5c9567d679bd
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-09 15:56:03 +0900

    Remove console exporter

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -1,5 +1,5 @@
 import { NodeTracerProvider } from '@opentelemetry/node';
-import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
@@ -8,7 +8,6 @@ import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-proto'
 console.log('Registering OpenTelemetry');
 
 const provider = new NodeTracerProvider();
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 // TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
 provider.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter()));
 
