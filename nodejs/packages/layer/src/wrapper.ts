67955a8f212359b3fcad52c1b217a607ce9b9f4d
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-12 16:30:03 +0900

    Add resource for nodejs.

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -2,14 +2,28 @@ import { NodeTracerProvider } from '@opentelemetry/node';
 import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-proto';
+import { CLOUD_RESOURCE, Resource } from '@opentelemetry/resources';
+import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
 
 console.log('Registering OpenTelemetry');
 
-const provider = new NodeTracerProvider();
+// TODO(anuraaga): Replace with detector after figuring out how to deal with async.
+// https://github.com/open-telemetry/opentelemetry-js/pull/2102/files#r611385113
+const resource = new Resource({
+  [CLOUD_RESOURCE.PROVIDER]: 'aws',
+  [CLOUD_RESOURCE.REGION]: process.env.AWS_REGION!,
+  'faas.name': process.env.AWS_LAMBDA_FUNCTION_NAME!,
+  'faas.version': process.env.AWS_LAMBDA_FUNCTION_VERSION!,
+});
+
+const provider = new NodeTracerProvider({
+  resource,
+});
 // TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
-provider.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter()));
+provider.addSpanProcessor(
+  new SimpleSpanProcessor(new CollectorTraceExporter())
+);
 
 registerInstrumentations({
   instrumentations: [
