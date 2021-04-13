ad99c6a949692261c2f909d39a5976a007fcd8c2
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-13 12:43:48 +0900

    Use upstream

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -3,36 +3,38 @@ import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-proto';
-import { CLOUD_RESOURCE, Resource } from '@opentelemetry/resources';
+import { awsLambdaDetector } from '@opentelemetry/resource-detector-aws';
 import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
 
 console.log('Registering OpenTelemetry');
 
-// TODO(anuraaga): Replace with detector after figuring out how to deal with async.
-// https://github.com/open-telemetry/opentelemetry-js/pull/2102/files#r611385113
-const resource = new Resource({
-  [CLOUD_RESOURCE.PROVIDER]: 'aws',
-  [CLOUD_RESOURCE.REGION]: process.env.AWS_REGION!,
-  'faas.name': process.env.AWS_LAMBDA_FUNCTION_NAME!,
-  'faas.version': process.env.AWS_LAMBDA_FUNCTION_VERSION!,
-});
+const instrumentations = [
+  new AwsInstrumentation({
+    suppressInternalInstrumentation: true,
+  }),
+  new AwsLambdaInstrumentation(),
+];
 
-const provider = new NodeTracerProvider({
-  resource,
-});
-// TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
-provider.addSpanProcessor(
-  new SimpleSpanProcessor(new CollectorTraceExporter())
-);
-
+// Register instrumentations synchronously to ensure code is patched even before provider is ready.
 registerInstrumentations({
-  instrumentations: [
-    new AwsInstrumentation({
-      suppressInternalInstrumentation: true,
-    }),
-    new AwsLambdaInstrumentation(),
-  ],
-  tracerProvider: provider,
+  instrumentations,
 });
 
-provider.register();
+async function initializeProvider() {
+  const resource = await awsLambdaDetector.detect();
+  const tracerProvider = new NodeTracerProvider({
+    resource,
+  });
+  // TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
+  tracerProvider.addSpanProcessor(
+    new SimpleSpanProcessor(new CollectorTraceExporter())
+  );
+  tracerProvider.register();
+
+  // Re-register instrumentation with initialized provider. Patched code will see the update.
+  registerInstrumentations({
+    instrumentations,
+    tracerProvider,
+  });
+}
+initializeProvider();
