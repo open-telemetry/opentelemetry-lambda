55e45fdaf4f3bf34f07c1aaec7794dc1b59f2b08
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-13 13:05:20 +0900

    Add resource for nodejs. (#30)
    * Add resource for nodejs.
    
    * Use upstream

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -2,23 +2,39 @@ import { NodeTracerProvider } from '@opentelemetry/node';
 import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-proto';
+import { awsLambdaDetector } from '@opentelemetry/resource-detector-aws';
+import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
 
 console.log('Registering OpenTelemetry');
 
-const provider = new NodeTracerProvider();
-// TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
-provider.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter()));
+const instrumentations = [
+  new AwsInstrumentation({
+    suppressInternalInstrumentation: true,
+  }),
+  new AwsLambdaInstrumentation(),
+];
 
+// Register instrumentations synchronously to ensure code is patched even before provider is ready.
 registerInstrumentations({
-  instrumentations: [
-    new AwsInstrumentation({
-      suppressInternalInstrumentation: true,
-    }),
-    new AwsLambdaInstrumentation(),
-  ],
-  tracerProvider: provider,
+  instrumentations,
 });
 
-provider.register();
+async function initializeProvider() {
+  const resource = await awsLambdaDetector.detect();
+  const tracerProvider = new NodeTracerProvider({
+    resource,
+  });
+  // TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
+  tracerProvider.addSpanProcessor(
+    new SimpleSpanProcessor(new CollectorTraceExporter())
+  );
+  tracerProvider.register();
+
+  // Re-register instrumentation with initialized provider. Patched code will see the update.
+  registerInstrumentations({
+    instrumentations,
+    tracerProvider,
+  });
+}
+initializeProvider();
