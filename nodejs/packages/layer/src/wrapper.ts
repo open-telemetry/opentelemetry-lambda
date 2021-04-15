f4e3589752f5b6d13c6ee48de9a9e399bc678694
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-15 16:13:00 +0900

    Add extension points for configuring NodeJS tracer

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -1,10 +1,20 @@
-import { NodeTracerProvider } from '@opentelemetry/node';
-import { SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { NodeTracerConfig, NodeTracerProvider } from '@opentelemetry/node';
+import {
+  SDKRegistrationConfig,
+  SimpleSpanProcessor,
+} from '@opentelemetry/tracing';
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-proto';
 import { awsLambdaDetector } from '@opentelemetry/resource-detector-aws';
 import { AwsInstrumentation } from 'opentelemetry-instrumentation-aws-sdk';
+
+declare global {
+  function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
+  function configureSdkRegistration(
+    defaultSdkRegistration: SDKRegistrationConfig
+  ): SDKRegistrationConfig;
+}
 
 console.log('Registering OpenTelemetry');
 
@@ -22,14 +32,25 @@ registerInstrumentations({
 
 async function initializeProvider() {
   const resource = await awsLambdaDetector.detect();
-  const tracerProvider = new NodeTracerProvider({
+
+  let config: NodeTracerConfig = {
     resource,
-  });
+  };
+  if (typeof configureTracer === 'function') {
+    config = configureTracer(config);
+  }
+
+  const tracerProvider = new NodeTracerProvider(config);
   // TODO(anuraaga): Switch to BatchSpanProcessor after using published instrumentation package.
   tracerProvider.addSpanProcessor(
     new SimpleSpanProcessor(new CollectorTraceExporter())
   );
-  tracerProvider.register();
+
+  let sdkRegistrationConfig: SDKRegistrationConfig = {};
+  if (typeof configureSdkRegistration === 'function') {
+    sdkRegistrationConfig = configureSdkRegistration(sdkRegistrationConfig);
+  }
+  tracerProvider.register(sdkRegistrationConfig);
 
   // Re-register instrumentation with initialized provider. Patched code will see the update.
   registerInstrumentations({
