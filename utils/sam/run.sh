b8556637f12a59fecbb2ef38311d85d6c731402c
Wang <wangzl@amazon.com>
2021-04-18 11:09:31 -0700

    allow sam layer published name can be configurable

--- utils/sam/run.sh
+++ utils/sam/run.sh
@@ -93,34 +93,15 @@ main() {
 	fi
 
 	if [[ $deploy == true ]]; then
-		if [[ $(is_sample) == "1" ]]; then
-			echo "sam deploying..."
-			sam deploy --stack-name $stack --region $region --capabilities CAPABILITY_NAMED_IAM --resolve-s3
-		else
-			echo "zip and awscli deploying..."
-			BUCKET_NAME="lambda-artifacts"-$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
-			echo "$BUCKET_NAME"
-			cd .aws-sam/build/OTelLayer && zip -q -r layer.zip *
-			aws s3 mb s3://$BUCKET_NAME --region $region
-			aws s3 cp layer.zip s3://$BUCKET_NAME --region $region
-			aws lambda publish-layer-version --region $region --layer-name $layerName --content S3Bucket=$BUCKET_NAME,S3Key=layer.zip --compatible-runtimes nodejs12.x nodejs10.x java11 python3.8 python3.7 --query 'LayerVersionArn' --output text
-			aws s3 rm s3://$BUCKET_NAME/layer.zip --region $region
-			aws s3 rb s3://$BUCKET_NAME --region $region
-			rm layer.zip
-			cd ../../..
-		fi
+	  sam deploy --stack-name $stack --region $region --capabilities CAPABILITY_NAMED_IAM --resolve-s3 --parameter-overrides LayerName=$layerName
 		rm -rf otel/otel_collector
 		rm -rf .aws-sam
 	fi
 
 	if [[ $layer == true ]]; then
 		echo -e "\nOTel Lambda layer ARN:"
-		if [[ $(is_sample) == "1" ]]; then
-			echo $(aws cloudformation describe-stack-resources --stack-name $stack --region $region --query 'StackResources[0].PhysicalResourceId' --output text)
-		else
-			arn=$(aws lambda list-layer-versions --layer-name $layerName --region $region --query 'max_by(LayerVersions, &Version).LayerVersionArn')
-			echo $arn | sed 's/\"//g'
-		fi
+		arn=$(aws lambda list-layer-versions --layer-name $layerName --region $region --query 'max_by(LayerVersions, &Version).LayerVersionArn')
+		echo $arn | sed 's/\"//g'
 	fi
 }
 
