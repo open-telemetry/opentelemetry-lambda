6fa43b88b5b82621d7b8b51ab686fbafd0d39a0c
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-19 14:23:32 +0900

    Fix python build script can't be run without terminal

--- python/src/build.sh
+++ python/src/build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build
 docker build -t aws-otel-lambda-python-layer otel
-docker run -it --rm -v "$(pwd)/build:/out" aws-otel-lambda-python-layer
+docker run --rm -v "$(pwd)/build:/out" aws-otel-lambda-python-layer
