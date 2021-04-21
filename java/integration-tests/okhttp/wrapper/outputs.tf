97d7c7ce6ddae99739fda63f531def29a1128617
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-21 15:51:14 +0900

    Output function role namd and some output fixes. (#61)

--- java/integration-tests/okhttp/wrapper/outputs.tf
+++ java/integration-tests/okhttp/wrapper/outputs.tf
@@ -1,3 +1,7 @@
 output "api-gateway-url" {
-  value = module.hello-okhttp-function.api-gateway-url
+  value = module.hello-lambda-function.api-gateway-url
+}
+
+output "function_role_name" {
+  value = module.hello-lambda-function.function_role_name
 }
