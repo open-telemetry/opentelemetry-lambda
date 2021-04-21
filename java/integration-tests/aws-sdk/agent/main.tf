0a376a45e4091bb2e1b361eac223c5090c1e1f75
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-21 16:01:38 +0900

    Fix composition of integration tests by using path.module

--- java/integration-tests/aws-sdk/agent/main.tf
+++ java/integration-tests/aws-sdk/agent/main.tf
@@ -1,17 +1,17 @@
 resource "aws_lambda_layer_version" "sdk_layer" {
   layer_name          = var.sdk_layer_name
-  filename            = "../../../layer-javaagent/build/distributions/opentelemetry-javaagent-layer.zip"
+  filename            = "${path.module}/../../../layer-javaagent/build/distributions/opentelemetry-javaagent-layer.zip"
   compatible_runtimes = ["java8", "java8.al2", "java11"]
   license_info        = "Apache-2.0"
-  source_code_hash    = filebase64sha256("../../../layer-javaagent/build/distributions/opentelemetry-javaagent-layer.zip")
+  source_code_hash    = filebase64sha256("${path.module}/../../../layer-javaagent/build/distributions/opentelemetry-javaagent-layer.zip")
 }
 
 resource "aws_lambda_layer_version" "collector_layer" {
   layer_name          = var.collector_layer_name
-  filename            = "../../../../collector/build/collector-extension.zip"
+  filename            = "${path.module}/../../../../collector/build/collector-extension.zip"
   compatible_runtimes = ["nodejs10.x", "nodejs12.x", "nodejs14.x"]
   license_info        = "Apache-2.0"
-  source_code_hash    = filebase64sha256("../../../../collector/build/collector-extension.zip")
+  source_code_hash    = filebase64sha256("${path.module}/../../../../collector/build/collector-extension.zip")
 }
 
 module "hello-lambda-function" {
--- java/integration-tests/okhttp/wrapper/main.tf
+++ java/integration-tests/okhttp/wrapper/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_layer_version" "sdk_layer" {
   layer_name          = var.sdk_layer_name
-  filename            = "../../../layer-wrapper/build/distributions/opentelemetry-java-wrapper.zip"
+  filename            = "${path.module}/../../../layer-wrapper/build/distributions/opentelemetry-java-wrapper.zip"
   compatible_runtimes = ["java8", "java8.al2", "java11"]
   license_info        = "Apache-2.0"
   source_code_hash    = filebase64sha256("../../../layer-wrapper/build/distributions/opentelemetry-java-wrapper.zip")
@@ -8,7 +8,7 @@ resource "aws_lambda_layer_version" "sdk_layer" {
 
 resource "aws_lambda_layer_version" "collector_layer" {
   layer_name          = var.collector_layer_name
-  filename            = "../../../../collector/build/collector-extension.zip"
+  filename            = "${path.module}/../../../../collector/build/collector-extension.zip"
   compatible_runtimes = ["nodejs10.x", "nodejs12.x", "nodejs14.x"]
   license_info        = "Apache-2.0"
   source_code_hash    = filebase64sha256("../../../../collector/build/collector-extension.zip")
