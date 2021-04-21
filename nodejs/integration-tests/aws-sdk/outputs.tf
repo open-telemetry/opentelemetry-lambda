46e4f4e2cf0a7be4e7a67cb153504e565d9525b5
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-21 15:45:06 +0900

    Output function role namd and some output fixes.

--- nodejs/integration-tests/aws-sdk/outputs.tf
+++ nodejs/integration-tests/aws-sdk/outputs.tf
@@ -1,3 +1,7 @@
 output "api-gateway-url" {
-  value = module.hello-awssdk-function.api-gateway-url
+  value = module.hello-lambda-function.api-gateway-url
+}
+
+output "function_role_name" {
+  value = module.hello-lambda-function.function_role_name
 }
