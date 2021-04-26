207eff302a6775aaa10bfe2b350a4178ed85f835
Wang <wangzl@amazon.com>
2021-04-26 14:42:29 -0700

    update pip to pythong -m pip, fix pip install warning

--- python/sample-apps/build.sh
+++ python/sample-apps/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p build/python
-pip3 install -r function/requirements.txt -t build/python
+python3 -m pip install -r function/requirements.txt -t build/python
 cp function/lambda_function.py -t build/python
 cd build/python || exit
 zip -r ../function.zip *
