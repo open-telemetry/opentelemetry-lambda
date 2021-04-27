85e14057b03cf688c71bf9397b0c028d24ce5f53
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-27 09:30:11 +0900

    Output built layer arns

--- java/integration-tests/aws-sdk/agent/outputs.tf
+++ java/integration-tests/aws-sdk/agent/outputs.tf
@@ -5,3 +5,11 @@ output "api-gateway-url" {
 output "function_role_name" {
   value = module.hello-lambda-function.function_role_name
 }
+
+output "collector_layer_arn" {
+  value = aws_lambda_layer_version.collector_layer.arn
+}
+
+output "sdk_layer_arn" {
+  value = aws_lambda_layer_version.sdk_layer.arn
+}
