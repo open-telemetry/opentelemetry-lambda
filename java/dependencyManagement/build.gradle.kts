ec35c2573ea23435276534d4ff7601d62ed7f499
Wang <wangzl@amazon.com>
2021-05-19 22:24:11 -0700

    bump java dependency to 1.2.0

--- java/dependencyManagement/build.gradle.kts
+++ java/dependencyManagement/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 data class DependencySet(val group: String, val version: String, val modules: List<String>)
 
 val DEPENDENCY_BOMS = listOf(
-    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.1.0-alpha",
+    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.2.0-alpha",
     "io.grpc:grpc-bom:1.37.0",
     "org.apache.logging.log4j:log4j-bom:2.14.0",
     "software.amazon.awssdk:bom:2.15.67"
@@ -19,7 +19,7 @@ val DEPENDENCIES = listOf(
     "com.amazonaws:aws-lambda-java-core:1.2.1",
     "com.amazonaws:aws-lambda-java-events:3.7.0",
     "com.squareup.okhttp3:okhttp:4.9.0",
-    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.1.0"
+    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.2.0"
 )
 
 javaPlatform {
