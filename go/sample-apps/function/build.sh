6bf141fa072c7d1a44e1cf3709a069b4788b7fb5
Garrett Egan <22334769+garrettwegan@users.noreply.github.com>
2021-07-13 17:23:55 -0700

    Add go support

--- go/sample-apps/function/build.sh
+++ go/sample-apps/function/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-mkdir -p build/go
-dotnet publish --output "./build/dotnet" --configuration "Release" --framework "netcoreapp3.1" /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64 --self-contained false
-GOOS=linux GOARCH=amd64 go build -o build/go/bootstrap .
-cd build/go || exit
+mkdir -p build
+GOOS=linux GOARCH=amd64 go build -o ./build/bootstrap .
+cd build || exit
 zip bootstrap.zip bootstrap
