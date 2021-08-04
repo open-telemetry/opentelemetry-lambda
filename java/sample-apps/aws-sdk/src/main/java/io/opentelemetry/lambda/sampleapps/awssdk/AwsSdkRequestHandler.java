fabaf92edc6ee996b0332cfd817cf62ebf906721
xyira <xuxx2170@mylaurier.ca>
2021-08-04 03:24:08 -0400

    Add metric to AWS SDK sample app  (#127)
    * add metric to sample app
    
    * reorganize sample app
    
    * remove print statement
    
    * rename metric
    
    * Update java/sample-apps/aws-sdk/src/main/java/io/opentelemetry/lambda/sampleapps/awssdk/AwsSdkRequestHandler.java
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>
    
    Co-authored-by: Karen Xu <karenyrxu@gmail.com>
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- java/sample-apps/aws-sdk/src/main/java/io/opentelemetry/lambda/sampleapps/awssdk/AwsSdkRequestHandler.java
+++ java/sample-apps/aws-sdk/src/main/java/io/opentelemetry/lambda/sampleapps/awssdk/AwsSdkRequestHandler.java
@@ -4,6 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.opentelemetry.api.metrics.GlobalMeterProvider;
+import io.opentelemetry.api.metrics.LongUpDownCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.common.Labels;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -13,6 +17,13 @@ public class AwsSdkRequestHandler
     implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
   private static final Logger logger = LogManager.getLogger(AwsSdkRequestHandler.class);
+  private static final Meter sampleMeter = GlobalMeterProvider.getMeter("aws-otel", "1.0");
+  private static final LongUpDownCounter queueSizeCounter =
+      sampleMeter
+          .longUpDownCounterBuilder("queueSizeChange")
+          .setDescription("Queue Size change")
+          .setUnit("one")
+          .build();
 
   @Override
   public APIGatewayProxyResponseEvent handleRequest(
@@ -25,6 +36,10 @@ public class AwsSdkRequestHandler
       response.setBody(
           "Hello lambda - found " + listBucketsResponse.buckets().size() + " buckets.");
     }
+
+    // Generate a sample counter metric using the OpenTelemetry Java Metrics API
+    queueSizeCounter.add(2, Labels.of("apiName", "apiName", "statuscode", "200"));
+
     return response;
   }
 }
