57feb8174f925000f95e4ce5faef46a8aab133fd
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-08-31 08:45:40 +0900

    Update Java to OTel 1.5 (#137)

--- java/settings.gradle.kts
+++ java/settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
         id("com.diffplug.spotless") version "5.8.2"
-        id("com.github.ben-manes.versions") version "0.36.0"
+        id("com.github.ben-manes.versions") version "0.39.0"
         id("com.github.johnrengelman.shadow") version "6.1.0"
     }
 }
