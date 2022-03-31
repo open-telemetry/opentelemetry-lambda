f1b62f120168a7639eb9953976999ce7c0f7fd2e
Vasi Vasireddy <41936996+vasireddy99@users.noreply.github.com>
2022-03-31 15:55:32 -0700

    Add support for Python 3.9 Layers (#218)

--- python/integration-tests/aws-sdk/wrapper/variables.tf
+++ python/integration-tests/aws-sdk/wrapper/variables.tf
@@ -33,3 +33,9 @@ variable "architecture" {
   description = "Lambda function architecture, valid values are arm64 or x86_64"
   default     = "x86_64"
 }
+
+variable "runtime" {
+  type        = string
+  description = "Python runtime version used for sample Lambda Function"
+  default     = "python3.9"
+}
