1c36b9b450a200c2970868f50a0b6e6b5b83012a
Vasi Vasireddy <41936996+vasireddy99@users.noreply.github.com>
2022-05-19 11:44:47 -0700

    Lambda Layer Update : OTel Java 1.14.0 (#240)

--- java/dependencyManagement/build.gradle.kts
+++ java/dependencyManagement/build.gradle.kts
@@ -9,16 +9,16 @@ plugins {
 data class DependencySet(val group: String, val version: String, val modules: List<String>)
 
 val DEPENDENCY_BOMS = listOf(
-    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.13.1-alpha",
+    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.14.0-alpha",
     "org.apache.logging.log4j:log4j-bom:2.17.2",
-    "software.amazon.awssdk:bom:2.17.179"
+    "software.amazon.awssdk:bom:2.17.193"
 )
 
 val DEPENDENCIES = listOf(
     "com.amazonaws:aws-lambda-java-core:1.2.1",
     "com.amazonaws:aws-lambda-java-events:3.11.0",
     "com.squareup.okhttp3:okhttp:4.9.3",
-    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.13.1"
+    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.14.0"
 )
 
 javaPlatform {
