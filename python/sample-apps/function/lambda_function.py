37b958bd46b5acadb821544cb7ae112b41aad9f7
Raphael Philipe Mendes da Silva <rapphil@gmail.com>
2022-09-07 10:03:24 -0700

    Use requests for python sample app (#280)
    * Use requests for python sample app
    
    * Update target endpoint of python sample app

--- python/sample-apps/function/lambda_function.py
+++ python/sample-apps/function/lambda_function.py
@@ -1,26 +1,14 @@
 import os
 import json
-import aiohttp
-import asyncio
 import boto3
-
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
-
-
-async def callAioHttp():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, "http://httpbin.org/")
+import requests
 
 s3 = boto3.resource("s3")
 
 # lambda function
 def lambda_handler(event, context):
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(callAioHttp())
+    requests.get("http://httpbin.org/")
 
     for bucket in s3.buckets.all():
         print(bucket.name)
