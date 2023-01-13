5d5a30be267e42a1456c735077d209a847015d5b
Alex Boten <aboten@lightstep.com>
2023-01-13 08:45:17 -0800

    [chore] add install-tools make target (#416)
    This will allow us to enable linting/fmt in the CI.
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

---
+++ collector/internal/tools/tools.go
@@ -0,0 +1,36 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build tools
+// +build tools
+
+package tools // import "go.opentelemetry.io/otel/internal/tools"
+
+import (
+	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/google/addlicense"
+	_ "github.com/jcchavezs/porto/cmd/porto"
+	_ "github.com/ory/go-acc"
+	_ "github.com/pavius/impi/cmd/impi"
+	_ "github.com/tcnksm/ghr"
+	_ "github.com/wadey/gocovmerge"
+	_ "go.opentelemetry.io/build-tools/checkdoc"
+	_ "go.opentelemetry.io/build-tools/chloggen"
+	_ "go.opentelemetry.io/build-tools/crosslink"
+	_ "go.opentelemetry.io/build-tools/multimod"
+	_ "go.opentelemetry.io/build-tools/semconvgen"
+	_ "golang.org/x/exp/cmd/apidiff"
+	_ "golang.org/x/tools/cmd/goimports"
+)
