42b352f59db15acd2c9abee7e16659809997c822
Huy Vo <hvoam@amazon.com>
2023-05-18 00:54:46 -0700

    Update Java version

--- java/dependencyManagement/build.gradle.kts
+++ java/dependencyManagement/build.gradle.kts
@@ -9,16 +9,16 @@ plugins {
 data class DependencySet(val group: String, val version: String, val modules: List<String>)
 
 val DEPENDENCY_BOMS = listOf(
-    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.25.0-alpha",
+    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.26.0-alpha",
     "org.apache.logging.log4j:log4j-bom:2.20.0",
-    "software.amazon.awssdk:bom:2.20.61"
+    "software.amazon.awssdk:bom:2.20.65"
 )
 
 val DEPENDENCIES = listOf(
     "com.amazonaws:aws-lambda-java-core:1.2.2",
     "com.amazonaws:aws-lambda-java-events:3.11.1",
     "com.squareup.okhttp3:okhttp:4.10.0",
-    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.25.0"
+    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.26.0"
 )
 
 javaPlatform {
