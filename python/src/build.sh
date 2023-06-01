8b973156e09ec42c74cf9dd14c0c648cbe75d4a2
Alex Boten <aboten@lightstep.com>
2023-06-01 08:36:07 -0700

    [chore] update python release to use build.sh
    This is creating the zip file without the sam metadata.
    
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- python/src/build.sh
+++ python/src/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 mkdir -p build
-docker build -t aws-otel-lambda-python-layer otel
+docker build --progress plain -t aws-otel-lambda-python-layer otel
 docker run --rm -v "$(pwd)/build:/out" aws-otel-lambda-python-layer
