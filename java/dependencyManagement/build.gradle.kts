0bd84f8c8d5c63b3e567654b36613f50f2df5735
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-07-14 11:00:56 -0700

    Update OTel Java Dependencies v0.28.0

--- java/dependencyManagement/build.gradle.kts
+++ java/dependencyManagement/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 data class DependencySet(val group: String, val version: String, val modules: List<String>)
 
 val DEPENDENCY_BOMS = listOf(
-    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.26.0-alpha",
+    "io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.28.0-alpha",
     "org.apache.logging.log4j:log4j-bom:2.20.0",
     "software.amazon.awssdk:bom:2.20.69"
 )
@@ -18,7 +18,7 @@ val DEPENDENCIES = listOf(
     "com.amazonaws:aws-lambda-java-core:1.2.2",
     "com.amazonaws:aws-lambda-java-events:3.11.2",
     "com.squareup.okhttp3:okhttp:4.11.0",
-    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.26.0"
+    "io.opentelemetry.javaagent:opentelemetry-javaagent:1.28.0"
 )
 
 javaPlatform {
