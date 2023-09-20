5e3c5709f2993dd3f026670b336de0d7f4a6205d
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-09-20 17:13:23 -0400

    update zip_ruby_layer script for passing name

--- ruby/README.md
+++ ruby/README.md
@@ -37,7 +37,7 @@ zip -qr ../../../<your_layer_name>.zip ruby/
 cd -
 
 # or run following script
-zip_ruby_layer.sh
+zip_ruby_layer.sh -n <your_layer_name>
 ```
 
 ### Why build layer this way
--- ruby/src/zip_ruby_layer.sh
+++ ruby/src/zip_ruby_layer.sh
@@ -1,5 +1,47 @@
 #!/bin/sh
 
-cd .aws-sam/build/OTelLayer/
-zip -qr ../../../<your_layer_name>.zip ruby/
-cd -
+set -e
+set -u
+
+echo_usage() {
+	echo "usage: Build Lambda layer/application by SAM"
+	echo " -n <specify layer name>"
+}
+
+is_sample() {
+	if [[ $(pwd) == *"sample"* ]]; then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+main() {
+	echo "running..."
+	layerName="otel-ruby-layer"
+
+	while getopts "n:h" opt; do
+		case "${opt}" in
+		h)
+			echo_usage
+			exit 0
+			;;
+		n)
+			layerName="${OPTARG}"
+			;;
+		\?)
+			exit 1
+			;;
+		:)
+			echo "Option -${OPTARG} requires an argument" >&2
+			exit 1
+			;;
+		esac
+	done
+
+	cd .aws-sam/build/OTelLayer/
+	zip -qr ../../../"$layerName".zip ruby/
+	cd -
+}
+
+main "$@"
