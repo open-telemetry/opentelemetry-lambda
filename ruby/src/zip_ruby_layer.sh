4471945aebc87f61f9ed4e936173114e1bb798d9
Xuan <112967240+xuan-cao-swi@users.noreply.github.com>
2024-01-26 15:23:27 -0500

    Adding ruby lambda layer (#918)
    * add ruby lambda layer draft
    
    * update zip_ruby_layer script for passing name
    
    * revision
    
    * revision and add more info on readme
    
    * add github action to build ruby lambda layer
    
    * use https instead of http for rubygems.org
    
    * add dependent bot
    
    * Update .github/dependabot.yml
    
    Co-authored-by: Tristan Sloughter <t@crashfast.com>
    
    * add sample handler as default runtime wrapper
    
    * Update ruby/README.md
    
    Co-authored-by: Tristan Sloughter <t@crashfast.com>
    
    * Update ruby/README.md
    
    Co-authored-by: Tristan Sloughter <t@crashfast.com>
    
    * fix workflow
    
    ---------
    
    Co-authored-by: Tristan Sloughter <t@crashfast.com>

---
+++ ruby/src/zip_ruby_layer.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+set -u
+
+echo_usage() {
+	echo "usage: Build Lambda layer/application by SAM"
+	echo " -n <specify layer name>"
+}
+
+
+main() {
+	echo "running..."
+	layerName="opentelemetry-ruby-layer"
+
+	while getopts "n:h" opt; do
+		case "${opt}" in
+		h)
+			echo_usage
+			exit 0
+			;;
+		n)
+			layerName="${OPTARG}"
+			;;
+		\?)
+			exit 1
+			;;
+		:)
+			echo "Option -${OPTARG} requires an argument" >&2
+			exit 1
+			;;
+		esac
+	done
+
+	cd .aws-sam/build/OTelLayer/
+	zip -qr ../../../"$layerName".zip ruby/
+	cd -
+}
+
+main "$@"
