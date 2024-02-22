d39d7c53908f0d61862d05dde1ac1f52061f6f3a
Jerry Leung <tingfung.leung@solarwinds.com>
2024-02-21 16:11:29 -0800

    Fixed the zap logger always use info as default

---
+++ collector/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"testing"
+)
+
+func TestInitLogger(t *testing.T) {
+	logger := initLogger()
+	assert.Equal(t, zapcore.WarnLevel, logger.Level())
+}
+
+func TestInitLogger_WithEnv(t *testing.T) {
+	os.Setenv("OPENTELEMETRY_EXTENSION_LOG_LEVEL", "debug")
+	logger := initLogger()
+	assert.Equal(t, zapcore.DebugLevel, logger.Level())
+	os.Unsetenv("OPENTELEMETRY_EXTENSION_LOG_LEVEL")
+}
