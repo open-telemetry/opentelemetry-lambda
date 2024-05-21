4c325830d28de110bfe8bbf8ce10c81042c66580
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-05-21 11:54:30 -0400

    feat: revision

--- ruby/src/layer/wrapper.rb
+++ ruby/src/layer/wrapper.rb
@@ -6,9 +6,6 @@ OpenTelemetry::SDK.configure do |c|
   c.use 'OpenTelemetry::Instrumentation::AwsLambda'
 end
 
-::OpenTelemetry::Instrumentation.registry.register('OpenTelemetry::Instrumentation::AwsLambda')
-::OpenTelemetry::Instrumentation.registry.install(['OpenTelemetry::Instrumentation::AwsLambda'])
-
 def otel_wrapper(event:, context:)
   otel_wrapper = OpenTelemetry::Instrumentation::AwsLambda::Handler.new()
   otel_wrapper.call_wrapped(event: event, context: context)
