8d8e317e2c57abf11a12837b55aae67c18ce2209
Serkan ÖZAL <sozal@catchpoint.com>
2024-11-11 05:28:47 +0300

    Activate ESM loader hook of the "import-in-the-middle" library for ES (EcmaScript) based user handlers (#1574)
    * Activate ESM loader hook of the "import-in-the-middle" library for ES (EcmaScript) based user handlers
    
    * Add tests

---
+++ nodejs/packages/layer/test/handler/esm/index.js
@@ -0,0 +1,4 @@
+export async function handler() {
+  console.log("I am a CommonJS module");
+  return 'ok';
+}
--- nodejs/packages/layer/test/wrapper.spec.ts
+++ nodejs/packages/layer/test/wrapper.spec.ts
@@ -1,6 +1,8 @@
 import type { AwsSdkInstrumentationConfig } from '@opentelemetry/instrumentation-aws-sdk';
 import { stub } from 'sinon';
 
+import { wrap, unwrap } from '../src/wrapper';
+
 declare global {
   function configureAwsInstrumentation(
     defaultConfig: AwsSdkInstrumentationConfig,
@@ -10,14 +12,22 @@ declare global {
 const assert = require('assert');
 
 describe('wrapper', () => {
+  beforeEach(() => {
+    unwrap();
+  });
+
+  afterEach(() => {
+    unwrap();
+  });
+
   describe('configureAwsInstrumentation', () => {
     it('is used if defined', () => {
       const configureAwsInstrumentationStub = stub().returns({
         suppressInternalInstrumentation: true,
       });
       global.configureAwsInstrumentation = configureAwsInstrumentationStub;
-      require('../src/wrapper');
+      wrap();
       assert(configureAwsInstrumentationStub.calledOnce);
-    }).timeout(10000);
+    });
   });
 });
