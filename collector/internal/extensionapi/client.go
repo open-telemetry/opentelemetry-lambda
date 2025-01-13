bce5f1bde7bb9ba8e3c233243140047069bb60c5
Pavlos Karakalidis <pkarakal@pkarakal.com>
2025-01-13 17:58:53 +0200

    fix(extension): consider requests failed when status code is not 2xx

--- collector/internal/extensionapi/client.go
+++ collector/internal/extensionapi/client.go
@@ -180,7 +180,7 @@ func (e *Client) doRequest(req *http.Request, out interface{}) (*http.Response, 
 	if err != nil {
 		return nil, err
 	}
-	if resp.StatusCode != 200 {
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return nil, fmt.Errorf("request failed with status %s", resp.Status)
 	}
 	defer resp.Body.Close()
