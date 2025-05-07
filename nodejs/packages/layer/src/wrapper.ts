dcbdf00de3d35599bc57dd8e6250a42e13aeb744
Alexey Kalmakov <st00nsa@gmail.com>
2025-05-07 19:29:32 +0200

    NodeJS: Replace `SimpleLogRecordProcessor` by `BatchLogRecordProcessor` (#1801)
    * Replace SimpleLogRecordProcessor  by BatchLogRecordProcessor
    
    * use SimpleLogRecordProcessor for debug
    
    * fix lint

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -449,8 +449,12 @@ async function initializeLoggerProvider(
   }
 
   const { logs } = await import('@opentelemetry/api-logs');
-  const { LoggerProvider, SimpleLogRecordProcessor, ConsoleLogRecordExporter } =
-    await import('@opentelemetry/sdk-logs');
+  const {
+    LoggerProvider,
+    BatchLogRecordProcessor,
+    SimpleLogRecordProcessor,
+    ConsoleLogRecordExporter,
+  } = await import('@opentelemetry/sdk-logs');
   const { OTLPLogExporter } = await import(
     '@opentelemetry/exporter-logs-otlp-http'
   );
@@ -464,7 +468,7 @@ async function initializeLoggerProvider(
     configureLoggerProvider(loggerProvider);
   } else {
     loggerProvider.addLogRecordProcessor(
-      new SimpleLogRecordProcessor(logExporter),
+      new BatchLogRecordProcessor(logExporter),
     );
     logs.setGlobalLoggerProvider(loggerProvider);
   }
