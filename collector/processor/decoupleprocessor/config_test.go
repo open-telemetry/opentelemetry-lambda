4e78e1a68daa553aa864bd9851061347f38afd97
James Thompson <thompson.tomo@outlook.com>
2026-08-11 20:23:06 +1000

    switch test

--- collector/processor/decoupleprocessor/config_test.go
+++ collector/processor/decoupleprocessor/config_test.go
@@ -20,7 +20,8 @@ import (
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
-	"go.opentelemetry.io/collector/confmap/xconfmap"
+	"go.opentelemetry.io/collector/component"
+    "go.opentelemetry.io/collector/confmap"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -90,7 +91,7 @@ func TestLoadConfig(t *testing.T) {
 			require.NoError(t, err)
 			require.NoError(t, sub.Unmarshal(cfg))
 
-			assert.NoError(t, xconfmap.Validate(cfg))
+			assert.NoError(t, cfg.Validate())
 			assert.Equal(t, tt.expected, cfg)
 		})
 	}
--- collector/receiver/telemetryapireceiver/config_test.go
+++ collector/receiver/telemetryapireceiver/config_test.go
@@ -22,7 +22,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
-	"go.opentelemetry.io/collector/confmap/xconfmap"
+	"go.opentelemetry.io/collector/component"
+    "go.opentelemetry.io/collector/confmap"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -111,7 +112,7 @@ func TestLoadConfig(t *testing.T) {
 			sub, err := cm.Sub(tt.id.String())
 			require.NoError(t, err)
 			require.NoError(t, sub.Unmarshal(cfg))
-			require.NoError(t, xconfmap.Validate(cfg))
+			require.NoError(t, cfg.Validate())
 
 			require.Equal(t, tt.expected, cfg)
 		})
